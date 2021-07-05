7d7e3043661804f85b4061e3e7927055ec6dac7f
Richard Albert Leddy <richardaleddy@gmail.com>
2021-07-05 16:20:55 -0700

    Make global persistence into its own thing
    IPFS, Relays, and Share Mem LRU all manage abstract data sharing and specific (named) message queues.

---
+++ index.js
@@ -0,0 +1,10 @@
+const PersistenceManager = require('./lib/global_persistence')
+const InterPlanetaryContactServices = require('./lib/ifps_services')
+
+
+
+
+
+
+module.exports.PersistenceManager = PersistenceManager
+module.exports.InterPlanetaryContactServices = InterPlanetaryContactServices
