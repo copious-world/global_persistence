ed890851e76eb0c4d72480a00260a1c7b37b86ca
Richard Albert Leddy <richardaleddy@gmail.com>
2022-04-19 11:11:15 -0700

    Adding a generalized connection to categorical handlers

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const PersistenceManager = require('./lib/global_persistence')
 const InterPlanetaryContactServices = require('./lib/ifps_services')
+const CategoricalPersistenceManager = require('./lib/global_categories')
 
 
 
 
 
 
+module.exports.CategoricalPersistenceManager = CategoricalPersistenceManager
 module.exports.PersistenceManager = PersistenceManager
 module.exports.InterPlanetaryContactServices = InterPlanetaryContactServices
