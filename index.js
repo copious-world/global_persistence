e4e40511861180e2e1468d54c4b9d2fe6475cb71
Richard Albert Leddy <richardaleddy@gmail.com>
2022-07-16 15:27:11 -0700

    Expose some methods for the user that worked for NW app.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const PersistenceManager = require('./lib/global_persistence')
 const InterPlanetaryContactServices = require('./lib/ifps_services')
 const CategoricalPersistenceManager = require('./lib/global_categories')
+const CategoricalUserManager = require('./lib/global_category_user')
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-
-
-
-
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 module.exports.CategoricalPersistenceManager = CategoricalPersistenceManager
 module.exports.PersistenceManager = PersistenceManager
 module.exports.InterPlanetaryContactServices = InterPlanetaryContactServices
+module.exports.CategoricalUserManager = CategoricalUserManager
