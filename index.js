5ecb65f1a1f5df8628f168b55f85136e32aa4a0d
Richard Albert Leddy <richardaleddy@gmail.com>
2022-08-09 14:05:51 -0700

    A number of quick fixes for bringing up an app
    Needs more work, surely.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const PersistenceManager = require('./lib/global_persistence')
-const InterPlanetaryContactServices = require('./lib/ifps_services')
+//const InterPlanetaryContactServices = require('./lib/ipfs_services')
 const CategoricalPersistenceManager = require('./lib/global_categories')
 const CategoricalUserManager = require('./lib/global_category_user')
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
@@ -9,5 +9,5 @@ const CategoricalUserManager = require('./lib/global_category_user')
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 module.exports.CategoricalPersistenceManager = CategoricalPersistenceManager
 module.exports.PersistenceManager = PersistenceManager
-module.exports.InterPlanetaryContactServices = InterPlanetaryContactServices
+//module.exports.InterPlanetaryContactServices = InterPlanetaryContactServices
 module.exports.CategoricalUserManager = CategoricalUserManager
